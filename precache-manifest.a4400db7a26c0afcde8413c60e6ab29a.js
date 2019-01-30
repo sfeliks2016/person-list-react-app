8870bcf2e79a271866657a73fbca0733ecabe2cd
root <root@debian.debian.local>
2019-01-30 17:49:00 +0100

    Updates

---
+++ precache-manifest.a4400db7a26c0afcde8413c60e6ab29a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0704fecc9d8924484fec",
+    "url": "/person-list-react-app/static/js/runtime~main.0704fecc.js"
+  },
+  {
+    "revision": "aca77d6229f55cbae6dd",
+    "url": "/person-list-react-app/static/js/main.aca77d62.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/person-list-react-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "aca77d6229f55cbae6dd",
+    "url": "/person-list-react-app/static/css/main.06c664fc.chunk.css"
+  },
+  {
+    "revision": "dd64ee3b47d9fcdff07ff13babc1b726",
+    "url": "/person-list-react-app/index.html"
+  }
+];
